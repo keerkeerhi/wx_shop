5663f0f3db857f1f838a1b8befe2ba046f45dd21
mvlyforever <123278392@qq.com>
2019-12-24 16:41:52 +0800

    1224

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,16 +4,23 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    isFocus:false,
+    searchWords: '',
+    banners:[1,2,3],
+    likes:[
+      {name:'金鸽瓜子',price:6},
+      { name: '可口可乐', price: 6 },
+      { name: '箭牌口香糖', price: 6 },
+      { name: '旺旺牛奶糖', price: 6 },
+      { name: '阿尔卑斯牛奶糖', price: 6 }
+    ]
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  beFocus() {
+    this.setData({isFocus:true})
+  },
+  beNoFoc() {
+    this.setData({ isFocus:false })
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
@@ -49,6 +56,19 @@ Page({
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+  bindKeyInput(e) {
+    this.setData({
+      searchWords: e.detail.value
+    })
+  },
+  doSearch(){
+    wx.navigateTo({
+      url: '/pages/result/result',
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
     })
   }
 })
