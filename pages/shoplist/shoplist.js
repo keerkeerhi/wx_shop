711b85ca586d98ae2b0d56bbc713384623a34c86
mvlyforever <123278392@qq.com>
2019-12-26 14:29:26 +0800

    1226

--- pages/shoplist/shoplist.js
+++ pages/shoplist/shoplist.js
@@ -1,30 +1,52 @@
-// pages/shop/shop.js
+// pages/shoplist/shoplist.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    activeKey: 0
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
-  onChange(event) {
-    wx.showToast({
-      icon: 'none',
-      title: `切换至第${event.detail}项`
-    });
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
 
   },
 
