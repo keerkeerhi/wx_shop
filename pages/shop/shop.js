b1add954a9e868537033a39a567db1f2443bd5cc
mvlyforever <123278392@qq.com>
2019-12-30 16:41:05 +0800

    update 1230

--- pages/shop/shop.js
+++ pages/shop/shop.js
@@ -5,7 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-    activeKey: 0
+    activeKey: 0,
+    goods:[{
+      title:'营养快线',
+      price: 11
+    },
+      {
+        title: '营养快线',
+        price: 11
+      },
+      {
+        title: '营养快线',
+        price: 11
+      },
+      {
+        title: '营养快线',
+        price: 11
+      }, {
+        title: '营养快线',
+        price: 11
+      }, {
+        title: '营养快线',
+        price: 11
+      }]
   },
 
   /**
@@ -13,6 +35,9 @@ Page({
    */
   onLoad: function (options) {
     
+  },
+  toCar(e){
+    console.log('----catch',e)
   },
   onChange(event) {
     wx.showToast({
